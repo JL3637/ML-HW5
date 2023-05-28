387c3fe561f92366e3b6919a59f0b51cef3f4bef
jason <jason36375667@gmail.com>
2023-05-28 22:04:07 +0800

    good

--- Q17~20.py
+++ Q17~20.py
@@ -1,38 +1,38 @@
 import numpy as np
 import random
 import math
+from tqdm import tqdm
 
 def decision_stump(t_arr, u_arr):
-    s_id_theta_g = []
+    s_id_theta = []
     s, id, theta = -1, -1, -1
-    INF = 100
+    INF = 10000
     n = len(t_arr)
     m = 16
     g = 1
     for k in range(m):
-        t_arr = t_arr[t_arr[:, k+1].argsort()]
-        u_arr = u_arr[t_arr[:, k+1].argsort()]
+        z = t_arr[:, k+1].argsort()
+        t_arr = t_arr[z]
+        u_arr = u_arr[z]
         for i in [1, -1]:
             tmp_g = 0
             for j in range(n):
                 if t_arr[j][0] != i:
                     tmp_g += u_arr[j]
-            for j in range(n):
-                if tmp_g < g:
-                    g, s, id = tmp_g, i, k+1
-                    if j > 0:
-                        theta = (t_arr[j-1][k+1]+t_arr[j][k+1])/2
-                    else:
-                        theta = -INF
+            if tmp_g < g:
+                g, s, id, theta = tmp_g, i, k+1, -INF
+            for j in range(n-1):
                 if t_arr[j][0] == i:
                     tmp_g += u_arr[j]
                 else:
                     tmp_g -= u_arr[j]
-    s_id_theta_g.append(s)
-    s_id_theta_g.append(id)
-    s_id_theta_g.append(theta)
-    s_id_theta_g.append(g)
-    return s_id_theta_g
+                if tmp_g < g and t_arr[j][k+1] != t_arr[j+1][k+1]:
+                    g, s, id = tmp_g, i, k+1
+                    theta = (t_arr[j][k+1]+t_arr[j+1][k+1])/2
+    s_id_theta.append(s)
+    s_id_theta.append(id)
+    s_id_theta.append(theta)
+    return s_id_theta
 
 def sign(a):
     if a >= 0:
@@ -75,25 +75,58 @@ with open('test.txt') as file:
             test_list.append(tmp)
 
 t_arr = np.array(train_list)
+tt_arr = np.array(train_list)
+test_arr = np.array(test_list)
 n = len(t_arr)
+nn = len(test_list)
 u_list = [1/n] * n
 u_arr = np.array(u_list)
 E_in_list = []
-for t in range(10):
-    s_id_theta_g = decision_stump(t_arr, u_arr)
-    s, id, theta, g = s_id_theta_g[0], s_id_theta_g[1], s_id_theta_g[2], s_id_theta_g[3]
-    print(s_id_theta_g)
-    print(np.sum(u_arr))
-    epsilon = g / np.sum(u_arr)
-    scal_factor = math.sqrt((1-epsilon)/epsilon)
-    t_arr = t_arr[t_arr[:, id].argsort()]
-    u_arr = u_arr[t_arr[:, id].argsort()]
+Ein_G_list = [0]*n
+Eout_G_list = [0]*nn
+for t in tqdm(range(1000)):
+    s_id_theta = decision_stump(t_arr, u_arr)
+    s, id, theta = s_id_theta[0], s_id_theta[1], s_id_theta[2]
+    z = t_arr[:, id].argsort()
+    t_arr = t_arr[z]
+    u_arr = u_arr[z]
     wrong_data = 0
+    epsilon = 0
+    for i in range(n):
+        if s * sign(t_arr[i][id] - theta) != t_arr[i][0]:
+            wrong_data += 1
+            epsilon += u_arr[i]
+    epsilon = epsilon / sum(u_arr)
+    scal_factor = math.sqrt((1-epsilon)/epsilon)
     for i in range(n):
         if s * sign(t_arr[i][id] - theta) == t_arr[i][0]:
             u_arr[i] = u_arr[i] / scal_factor
         else:
             u_arr[i] = u_arr[i] * scal_factor
-            wrong_data += 1
     E_in_list.append(wrong_data/n)
-print(E_in_list)
+    a_t = np.log(scal_factor)
+    for i in range(n):
+        Ein_G_list[i] += a_t * (s * sign(tt_arr[i][id] - theta))
+    for i in range(nn):
+        Eout_G_list[i] += a_t * (s * sign(test_arr[i][id] - theta))
+
+for i in range(n):
+    Ein_G_list[i] = sign(Ein_G_list[i])
+Ein_G = 0
+for i in range(n):
+    if Ein_G_list[i] != tt_arr[i][0]:
+        Ein_G += 1
+Ein_G = Ein_G / n
+
+for i in range(nn):
+    Eout_G_list[i] = sign(Eout_G_list[i])
+Eout_G = 0
+for i in range(nn):
+    if Eout_G_list[i] != test_arr[i][0]:
+        Eout_G += 1
+Eout_G = Eout_G / nn
+
+print(min(E_in_list))
+print(max(E_in_list))
+print(Ein_G)
+print(Eout_G)
